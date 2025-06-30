a66209f8d52bd9a354476d80b9d6bf6d6155d38e
qwer1304 <qwer1304@hotmail.com>
2025-06-30 08:21:13 +0300

    add mmd distance

--- make_cmnist.py
+++ make_cmnist.py
@@ -112,6 +112,7 @@ for d, dataset in tqdm(enumerate(datasets), leave=False, total=len(datasets)):
     all_labels = []
 
     for idx, (img_tensor, label) in tqdm(enumerate(dataset), desc=f"Dataset {datasets.environments[d]}", leave=False, total=len(dataset)):
+        label += 1 # Label is expected to be 1..N
         save_dir_domain_label = save_dir_domain + str(label.item()) + '/'
         os.makedirs(save_dir_domain_label, exist_ok=True)
         
