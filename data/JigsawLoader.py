d3c2461315d5e0e7a3410b536c32a1ed61c181b5
qwer1304 <qwer1304@hotmail.com>
2025-07-04 09:54:48 +0300

    end of debug

--- data/JigsawLoader.py
+++ data/JigsawLoader.py
@@ -354,7 +354,7 @@ class JigsawNewDataset(data.Dataset):
         framename = self.data_path + '/' + self.names[index]
         img = Image.open(framename).convert('RGB')
         # image, image_randaug, label, domain
-        if self.with_domain_label
+        if self.with_domain_label:
             return self._image_transformer(img), int(self.labels[index] - 1), int(self.domain_labels[index] - 1)
         else:
             return self._image_transformer(img), int(self.labels[index] - 1)
@@ -376,7 +376,7 @@ class JigsawTestNewDataset(JigsawNewDataset):
     def __getitem__(self, index):
         framename = self.data_path + '/' + self.names[index]
         img = Image.open(framename).convert('RGB')
-        if self.with_domain_label
+        if self.with_domain_label:
             return self._image_transformer(img), int(self.labels[index] - 1), int(self.domain_labels[index] - 1)
         else:
             return self._image_transformer(img), int(self.labels[index] - 1)
