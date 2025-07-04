5e9b0e15ceb4e2e759bd6216c70ecb194c0cf056
qwer1304 <qwer1304@hotmail.com>
2025-07-04 10:05:42 +0300

    debug checkpoint loading not working properly

--- main_gfnet.py
+++ main_gfnet.py
@@ -469,7 +469,7 @@ def main(args):
                 model_path = args.resume + "/" + args.target + str(args.seed) + "/checkpoint_last.pth"
             checkpoint = torch.load(model_path, map_location='cpu')
             checkpoint = {'model': checkpoint}
-        model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
+        model_without_ddp.load_state_dict(checkpoint['model'], strict=True)
         if not (args.eval or args.export_eval_features) and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
             optimizer.load_state_dict(checkpoint['optimizer'])
             print('lr scheduler will not be updated')
